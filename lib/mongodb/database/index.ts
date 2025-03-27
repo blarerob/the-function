52bc7a2aff762f984e7c7632bc57987a2c2db5ef
brobinson <brobinson@jhacorp.com>
2025-03-27 00:03:11 -0400

    changed db conn

--- lib/mongodb/database/index.ts
+++ lib/mongodb/database/index.ts
@@ -1,26 +1,15 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-interface GlobalWithMongoose extends NodeJS.WarningListener {
-    mongoose: {
-        conn: mongoose.Connection | null,
-        promise: Promise<mongoose.Connection> | null
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
     }
-}
+};
 
-const cached: { conn: mongoose.Connection | null, promise: Promise<mongoose.Connection> | null } = (global as unknown as GlobalWithMongoose).mongoose || { conn: null, promise: null };
-export const connectToDatabase = async () => {
-    if (cached.conn) return cached.conn;
-
-    if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
-
-    mongoose.connect(MONGODB_URI, {
-        dbName: 'jtown-events',
-        bufferCommands: false,
-    })
-
-    cached.conn = await cached.promise;
-
-    return cached.conn;
-}
+connectToDatabase().then(r => console.log(r,'Database connection successful')).catch(err => console.error('Database connection error:', err));
